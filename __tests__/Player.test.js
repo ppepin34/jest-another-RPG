bb909430510e2c80fc9e334867d1d2e9216ebed0
Phil Pepin <ppepin34@gmail.com>
2022-04-26 14:19:22 -0400

    can create player, added 2 potions to player

---
+++ __tests__/Player.test.js
@@ -0,0 +1,19 @@
+const { default: TestRunner } = require('jest-runner');
+const Player = require('../lib/Player.js');
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion.js');
+
+console.log(new Potion());
+
+test('creates a Player object', () => {
+    const player = new Player('Dave');
+
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
+});
